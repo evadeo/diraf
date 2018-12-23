3b57f57a7efaaea1b68c897faa7f8e24c0f95f3f
Nicolas Lugassy <nicolas.lugassy@epita.fr>
2018-12-23 22:59:52 +0100

    headers: adding documentation for class header file

--- src/error_struct.hh
+++ src/error_struct.hh
@@ -1,15 +1,33 @@
 #pragma once
 
+/**
+ * \struct Error
+ *
+ * \brief Error struct to regroup all information
+ *
+ * This struct was made in order to regroup multiple information 
+ * and simplify function return values.
+ *
+ */
+
 #include <cstddef>
 
 struct Error
 {
+    ///Integer representing the split value
     int split_value_;
+    ///Unsigned integer representing the index on which the split should be done
     size_t split_index_;
+    ///Float value representing the error of the left split
     float err_left_;
+    ///Float value representing the error of the right split
     float err_right_;
+    ///Float value representing the total error for this split
     float err_total_;
 
+    /**
+     * \brief Constructor for the error class
+     */
     Error(int split_value = 0, size_t split_index = 0,
           float err_left = 1, float err_right = 1, float err_total = 1)
         : split_value_(split_value)
@@ -19,6 +37,9 @@ struct Error
         , err_total_(err_total)
     {}
 
+    /**
+     * \brief assignement operator
+     */
     Error& operator =(const Error& e)
     {
         split_value_ = e.split_value_;
@@ -29,6 +50,9 @@ struct Error
         return *this;
     }
 
+    /**
+     * \brief Lower than comparaison operator
+     */
     bool operator <(const Error& e) const
     {
         return err_total_ < e.err_total_;
--- src/tree.hh
+++ src/tree.hh
@@ -1,5 +1,14 @@
 #pragma once
 
+/**
+ * \class DecisionTree
+ *
+ * \brief Decision Tree class
+ *
+ * This class represents one Decision Tree. 
+ *
+ */
+
 #include <functional>
 #include <memory>
 #include <random>
@@ -9,35 +18,93 @@
 
 class DecisionTree
 {
+
+    /*
+     * \struct Node
+     *
+     * \brief Inner Node class
+     *
+     * This class represents one node of the Decision Tree.
+     * It will contain information on the split to perform and the possible 
+     * output class if the node is a leaf.
+     *
+     */
     struct Node
     {
+        /**
+         * \brief Constructor for the Node struct
+         * \param feature_index_split Integer that represents the index of the feature to perform the split on
+         * \param threshold Integer that will be the value to split on (if < then it will be on the left side, right side otherwise)
+         * \param label Integer that will indicated the output class if is a leaf
+         * \param is_leaf Boolean to tell if this node is a leaf or not
+         * 
+         */
         Node(int feature_index_split, int threshold, int label, bool is_leaf);
 
+        ///Left children
         std::shared_ptr<Node> left_;
+        ///Right children
         std::shared_ptr<Node> right_;
+        ///Integer representing the index of the feature to perform the split
         int feature_index_split_;
+        ///Integer that will be the value to split on
         int threshold_;
+        ///Integer representing the output class
         int label_;
+        ///Boolean to tell if the node is a leaf or not
         bool is_leaf_;
     };
 
     public:
+        /*
+         * \brief Empty constructor
+         */
         DecisionTree() : root_(nullptr) {}
+
+        /*
+         * \brief Constructor for the Decision Tree class
+         *
+         * \param features Matrix representing the elements and their associated features. 
+         *                 The elements are stored in line and the features are the columns.
+         * \param labels Vector representing the associated class to an element
+         * \param features_index Vector representing the real indexes of the features
+         *                       Because we only selected a few features previously,
+         *                       it may not be their original position.
+         * \param err_function Error function to minimize when doing a split
+         */
         DecisionTree(std::vector<std::vector<int>> features,
                 const std::vector<int>& labels,
                 std::vector<int> features_index,
                 const err_func& err_function);
-        void build_decision_tree(std::vector<std::vector<int>> features,
-                                 std::vector<int> labels);
 
+        /*
+         * \brief Function to predict the output class of an element.
+         *
+         * \param elem Vector containing all the features of one element.
+         */
         int predict(const std::vector<int>& elem) const;
 
     private:
-         std::shared_ptr<DecisionTree::Node> build_node(
+        /*
+         * \brief Function to recursively build the Decision Tree
+         *
+         * \param features Matrix representing the elements and their associated features. 
+         *                 The elements are stored in line and the features are the columns.
+         * \param labels Vector representing the associated class to an element
+         * \param features_index Vector representing the real indexes of the features
+         *                       Because we only selected a few features previously,
+         *                       it may not be their original position.
+         * \param err_function Error function to minimize when doing a split
+         *
+         * \return Returns the head of the Decision Tree
+         *
+         */
+        std::shared_ptr<DecisionTree::Node> build_node(
                 std::vector<std::vector<int>> features,
                 const std::vector<int>& labels,
                 std::vector<int> features_index,
                 const err_func& err_function);
         
+        ///Root of the Decision Tree
         std::shared_ptr<Node> root_;
 };
